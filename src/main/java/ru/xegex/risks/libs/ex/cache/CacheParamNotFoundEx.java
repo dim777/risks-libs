5bd8af56a10c64a9a28524be31dd5b9cee55d124
dim777 <dim777@ya.ru>
2017-10-06 16:50:47 +0300

    Add CacheNotFoundEx

---
+++ src/main/java/ru/xegex/risks/libs/ex/cache/CacheParamNotFoundEx.java
@@ -0,0 +1,12 @@
+package ru.xegex.risks.libs.ex.cache;
+
+import ru.xegex.risks.libs.ex.AppEx;
+
+/**
+ * Created by rb052775 on 06.10.2017.
+ */
+public class CacheParamNotFoundEx extends AppEx {
+    public CacheParamNotFoundEx(String message) {
+        super(message);
+    }
+}
